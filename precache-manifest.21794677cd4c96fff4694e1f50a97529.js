4e53bde9a1b81f43667bc77bb52ccdc7e5462c59
Salil786bhatnagar <68239654+Salil786bhatnagar@users.noreply.github.com>
2020-12-01 19:11:38 +0530

    Add files via upload

---
+++ precache-manifest.21794677cd4c96fff4694e1f50a97529.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640e16707aee0741d5690c105b6a92c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8fbab6bcb71cc8873b6",
+    "url": "/static/css/main.f89be5f9.chunk.css"
+  },
+  {
+    "revision": "c53a687a9a9710c802c0",
+    "url": "/static/js/2.b41a0942.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/static/js/2.b41a0942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fbab6bcb71cc8873b6",
+    "url": "/static/js/main.eb1b5519.chunk.js"
+  },
+  {
+    "revision": "97b60b1033c11fe972db",
+    "url": "/static/js/runtime-main.68ce33b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
